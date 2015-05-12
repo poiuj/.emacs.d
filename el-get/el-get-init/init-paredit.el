3a162ecafede0d1465caec5d622bc2cba00032cd
poiuj <poiuj@users.noreply.github.com>
2015-05-12 18:18:08 +0300

    Windows move

--- el-get/el-get-init/init-paredit.el
+++ el-get/el-get-init/init-paredit.el
@@ -5,6 +5,9 @@
   '(progn
      (define-key paredit-mode-map (kbd "C-<left>") nil)
      (define-key paredit-mode-map (kbd "C-<right>") nil)
+     (define-key paredit-mode-map (kbd "M-<up>") nil)
+     (define-key paredit-mode-map (kbd "M-<down>") nil)
+
      (define-key paredit-mode-map (kbd "M-w") 'paredit-wrap-sexp)
      (define-key paredit-mode-map (kbd "C-(") 'paredit-forward-barf-sexp)
      (define-key paredit-mode-map (kbd "C-)") 'paredit-forward-slurp-sexp)
--- global-keys.el
+++ global-keys.el
@@ -5,10 +5,12 @@
 
 (global-set-key (kbd "C-<left>") 'left-word)
 (global-set-key (kbd "C-<right>") 'right-word)
-(global-set-key (kbd "M-<left>") 'backward-sexp)
-(global-set-key (kbd "M-<right>") 'forward-sexp)
 
 (global-set-key (kbd "C-`") 'other-window)
+(global-set-key (kbd "M-<up>") 'windmove-up)
+(global-set-key (kbd "M-<down>") 'windmove-down)
+(global-set-key (kbd "M-<left>") 'windmove-left)
+(global-set-key (kbd "M-<right>") 'windmove-right)
 
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
 (global-set-key (kbd "C-r") 'isearch-backward-regexp)
