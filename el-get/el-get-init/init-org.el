8966fd0bfb92615bb8d756df7ce05cc33a38d6cb
poiuj <poiuj@users.noreply.github.com>
2015-05-13 13:45:01 +0300

    Additional key bindings for org-mode

--- el-get/el-get-init/init-org.el
+++ el-get/el-get-init/init-org.el
@@ -8,4 +8,6 @@
      (define-key org-mode-map (kbd "M-<right>") nil)
 
      (define-key org-mode-map (kbd "C-*") 'org-metaright)
-     (define-key org-mode-map (kbd "M-*") 'org-metaleft)))
+     (define-key org-mode-map (kbd "M-*") 'org-metaleft)
+     (define-key org-mode-map (kbd "C-M-u") 'org-metaup)
+     (define-key org-mode-map (kbd "C-M-j") 'org-metadown)))
