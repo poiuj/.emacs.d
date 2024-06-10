b12974db34d31f2ed31a31fbc70e9a8c69785540
poiuj <1099644+poiuj@users.noreply.github.com>
2024-06-10 13:59:30 +0300

    Migrate to keymap-global-set for global keys.

--- global-keys.el
+++ global-keys.el
@@ -1,42 +1,42 @@
 ;;; init for global keys
 
-(global-set-key (kbd "C-<down>") 'scroll-up-line)
-(global-set-key (kbd "C-<up>") 'scroll-down-line)
+(keymap-global-set "C-<down>" 'scroll-up-line)
+(keymap-global-set "C-<up>" 'scroll-down-line)
 
-(global-set-key (kbd "C-<left>") 'left-word)
-(global-set-key (kbd "C-<right>") 'right-word)
+(keymap-global-set "C-<left>" 'left-word)
+(keymap-global-set "C-<right>" 'right-word)
 
-(global-set-key (kbd "C-`") 'other-window)
-(global-set-key (kbd "M-<up>") 'windmove-up)
-(global-set-key (kbd "M-<down>") 'windmove-down)
-(global-set-key (kbd "M-<left>") 'windmove-left)
-(global-set-key (kbd "M-<right>") 'windmove-right)
+(keymap-global-set "C-`" 'other-window)
+(keymap-global-set "M-<up>" 'windmove-up)
+(keymap-global-set "M-<down>" 'windmove-down)
+(keymap-global-set "M-<left>" 'windmove-left)
+(keymap-global-set "M-<right>" 'windmove-right)
 
-(global-set-key (kbd "C-s") 'isearch-forward-regexp)
-(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(keymap-global-set "C-s" 'isearch-forward-regexp)
+(keymap-global-set "C-r" 'isearch-backward-regexp)
 
-(global-set-key (kbd "C-M-%") 'query-replace)
-(global-set-key (kbd "M-%") 'query-replace-regexp)
+(keymap-global-set "C-M-%" 'query-replace)
+(keymap-global-set "M-%" 'query-replace-regexp)
 
-(global-set-key (kbd "C-c n") 'my/clean-and-indent-buffer)
-(global-set-key (kbd "C-c q") 'my/escape-dbl-quotes)
+(keymap-global-set "C-c n" 'my/clean-and-indent-buffer)
+(keymap-global-set "C-c q" 'my/escape-dbl-quotes)
 
-(global-set-key (kbd "C-^") 'enlarge-window)
-(global-set-key (kbd "C-}") 'enlarge-window-horizontally)
+(keymap-global-set "C-^" 'enlarge-window)
+(keymap-global-set "C-}" 'enlarge-window-horizontally)
 
-(global-set-key (kbd "C-c p") 'my/pp-json)
+(keymap-global-set "C-c p" 'my/pp-json)
 
-(global-set-key (kbd "s-w") 'backward-kill-sexp)
+(keymap-global-set "s-w" 'backward-kill-sexp)
 
-(global-set-key (kbd "C-c u") 'browse-url)
+(keymap-global-set "C-c u" 'browse-url)
 
-(global-set-key (kbd "M-z") 'zap-up-to-char)
+(keymap-global-set "M-z" 'zap-up-to-char)
 
-(global-set-key (kbd "C-x M-w") 'my/save-file-path)
+(keymap-global-set "C-x M-w" 'my/save-file-path)
 
-(global-set-key (kbd "C-x M-u") 'my/upcase-last-sexp)
+(keymap-global-set "C-x M-u" 'my/upcase-last-sexp)
 
-(global-set-key (kbd "C-c s l") 'sort-lines)
+(keymap-global-set "C-c s l" 'sort-lines)
 
 (keymap-global-set "C-x C-b" 'buffer-menu-other-window)
 
