c4e1b1dfbeaaed9c97e5d3c0404a80cce01e7ece
poiuj <1099644+poiuj@users.noreply.github.com>
2024-08-26 22:01:06 +0300

    Removed unused utility function my/insert-installed-packages-list

--- utils.el
+++ utils.el
@@ -11,20 +11,6 @@
       (json-pretty-print (region-beginning) (region-end))
       (json-pretty-print (point-min) (point-max))))
 
-(defun my/insert-installed-packages-list ()
-  "Inserts quoted list of installed packages by el-get"
-  (interactive)
-  (insert ?')
-  (insert-parentheses)
-  (mapcar
-   (lambda (package)
-     (insert package)
-     (insert ?\s))
-   (el-get-list-package-names-with-status "installed"))
-  ;; delete the last space
-  (delete-backward-char 1)
-  (forward-char))
-
 (defun my/escape-dbl-quotes ()
   "Escapes double quotes in current region"
   (interactive)
