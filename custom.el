6c2b399706e19fc650cb9fbda135e18525b6544b
poiuj <1099644+poiuj@users.noreply.github.com>
2025-11-10 12:44:49 +0000

    Update installed packages in the custom file

--- custom.el
+++ custom.el
@@ -4,13 +4,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ag auto-package-update avy company company-mode consult
-        embark embark-consult exec-path-from-shell flycheck
-        git-gutter jq-mode lsp-mode lsp-pyright magit
-        marginalia monokai-theme multi-line multiple-cursors
-        mustache-mode orderless paredit plantuml-mode
-        rustic typescript-mode use-package vertico
-        yaml-mode yasnippet-snippets)))
+   '(ace-window ag auto-package-update avy consult
+                consult-lsp corfu corfu-terminal embark embark-consult
+                exec-path-from-shell flycheck git-gutter jq-mode
+                lsp-mode lsp-pyright magit marginalia monokai-theme
+                multi-line multiple-cursors mustache-mode orderless
+                paredit plantuml-mode rustic typescript-mode
+                use-package vertico yaml-mode yasnippet-snippets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
